f441e3e27fda09d9058fd5a1a64c8676a1cffd90
blackspade12 <blackspadejm@gmail.com>
2024-12-03 21:58:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import os
 
 # Hardcoded API key
 NEWS_API_KEY = "bed983842fff4c239027006d10fd8fc5"
@@ -50,4 +51,6 @@ def fetch_news():
         return jsonify(result), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get the PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
+
